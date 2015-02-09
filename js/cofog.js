0ecf28ccbef52363988424b5e68592345b7eaa73
chi-ba <yoshimgtsu@gmail.com>
2015-02-09 17:14:49 +0900

    Update cofog.js

--- js/cofog.js
+++ js/cofog.js
@@ -20,8 +20,8 @@ OpenSpending.Styles.Cofog = {
 			'02.1.3': { icon: 'icons/c_yougo.svg', color: '#C75746' },	/* B3 養護 */
 		'02.2': { icon: 'icons/education.svg', color: '#C75746' },			/* B4 教育 */
 		
-	'07': { icon: 'icons/c_waterdoun-watersup.svg', color: '#C75746' },			/* 上下水道 */
-		'07.1': { icon: 'icons/c_watersup.svg', color: '#C75746' },	/* G1 上水道 */
+	'07': { icon: 'icons/c_waterdoun-watersup.svg', color: '#C75746' },			/* 借金返済 */
+		'07.1': { icon: 'icons/c_watersup.svg', color: '#C75746' },	/* G1 公債費 */
 		'07.2': { icon: 'icons/c_waterdoun.svg', color: '#C75746' },	/* G2 下水道 */
 
 	'03': { icon: 'icons/economy-tourism.svg', color: '#C75746' },			/* 経済・観光 */
@@ -47,9 +47,10 @@ OpenSpending.Styles.Cofog = {
 		'08.1': { icon: 'c_shyoubou.svg', color: '#C75746' },		/* O1 消防・救急 */
 		'08.2': { icon: 'c_shyoubou.svg', color: '#C75746' },		/* O2 消防・救急 */
 		
-	'05': { icon: 'icons/environment.svg', color: '#C75746' },				/* 環境 */
-		'05.1': { icon: 'icons/tree.svg', color: '#C75746' },				/* E1 環境保全 */
-		'05.2': { icon: 'icons/garbage.svg', color: '#C75746' },			/* E2 資源循環 */
+	'05': { icon: 'icons/environment.svg', color: '#C75746' },				/* ごみ処理・リサイクル・環境保全 */
+		'05.1': { icon: 'icons/tree.svg', color: '#C75746' },				/* E1 ごみ処理 */
+		'05.2': { icon: 'icons/garbage.svg', color: '#C75746' },			/* E2 リサイクル */
+		'05.3': { icon: 'icons/garbage.svg', color: '#C75746' },			/* E3 環境保全*/
 		
 	'09': { icon: 'icons/c_government4.svg', color: '#C75746' },				/* その他 */
 		'09.1': { icon: 'icons/c_government4.svg', color: '#C75746' },		/* H1 その他 */
