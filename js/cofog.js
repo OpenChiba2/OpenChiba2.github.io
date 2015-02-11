b08f98a2286edd88e5177faf1c702ab14395955d
chi-ba <yoshimgtsu@gmail.com>
2015-02-11 17:14:32 +0900

    Update cofog.js

--- js/cofog.js
+++ js/cofog.js
@@ -12,12 +12,12 @@ OpenSpending.Styles.Cofog = {
 	'01': { icon: 'icons/c_kenkou_fukushi.svg', color: '#C75746' },			/* 保健・福祉・医療 */
 		'01.1': { icon: 'icons/c_kenkou.svg', color: '#C75746' },		/* A1 福祉 */
 			'01.1.1': { icon: 'icons/c_kosodate_shien.svg', color: '#C75746' },	/* B1 社会福祉 */
-			'01.1.2': { icon: 'icons/c_kosodate_shien.svg', color: '#C75746' },	/* B1 高齢者福祉 */
-			'01.1.3': { icon: 'icons/c_kosodate_shien.svg', color: '#C75746' },	/* B1 障害者福祉 */
+			'01.1.2': { icon: 'old-age.svg ', color: '#C75746' },	/* B1 高齢者福祉 */
+			'01.1.3': { icon: 'icons/helping-others.svg ', color: '#C75746' },	/* B1 障害者福祉 */
 			'01.1.4': { icon: 'icons/c_kosodate_shien.svg', color: '#C75746' },	/* B1 生活保護 */
 		'01.2': { icon: 'icons/c_fukushi.svg', color: '#C75746' },		/* A2 保健衛生 */
 			'01.2.1': { icon: 'icons/c_kosodate_shien.svg', color: '#C75746' },	/* B1 保健衛生 */
-			'01.2.2': { icon: 'icons/c_kosodate_shien.svg', color: '#C75746' },	/* B1 感染症・公害対策 */
+			'01.2.2': { icon: 'icons/pollution.svg ', color: '#C75746' },	/* B1 感染症・公害対策 */
 			'01.2.3': { icon: 'icons/c_kosodate_shien.svg', color: '#C75746' },	/* B1 環境衛生 */
 			'01.2.4': { icon: 'icons/hospital.svg ', color: '#C75746' },	/* B1 病院 */
 			'01.2.5': { icon: 'icons/c_watersup.svg ', color: '#C75746' },	/* B1 水道 */
@@ -31,43 +31,43 @@ OpenSpending.Styles.Cofog = {
 			'02.1.5': { icon: 'icons/c_yougo.svg', color: '#C75746' },	/* B3 保育園 */
 			'02.1.6': { icon: 'icons/c_yougo.svg', color: '#C75746' },	/* B3 保育所 */
 		'02.2': { icon: 'icons/education.svg', color: '#C75746' },			/* B4 教育 */
-			'02.2.1': { icon: 'icons/c_kosodate_shien.svg', color: '#C75746' },	/* B1 青少年育成 */
+			'02.2.1': { icon: 'icons/post-secondary.svg', color: '#C75746' },	/* B1 青少年育成 */
 			'02.2.2': { icon: 'icons/c_sankaku_ikusei.svg', color: '#C75746' },	/* B2 教育総務 */
-			'02.2.3': { icon: 'icons/c_yougo.svg', color: '#C75746' },	/* B3 教育指導 */
-			'02.2.4': { icon: 'icons/c_yougo.svg', color: '#C75746' },	/* B3 小学校 */
-			'02.2.5': { icon: 'icons/c_yougo.svg', color: '#C75746' },	/* B3 中学校 */
-			'02.2.6': { icon: 'icons/c_yougo.svg', color: '#C75746' },	/* B3 高等学校 */
-			'02.2.7': { icon: 'icons/c_yougo.svg', color: '#C75746' },	/* B3 特別支援学校 */
-			'02.2.8': { icon: 'icons/c_yougo.svg', color: '#C75746' },	/* B3 社会教育 */
+			'02.2.3': { icon: 'icons/research.svg', color: '#C75746' },	/* B3 教育指導 */
+			'02.2.4': { icon: 'icons/secondary-lower.svg ', color: '#C75746' },	/* B3 小学校 */
+			'02.2.5': { icon: 'icons/post-secondary.svg ', color: '#C75746' },	/* B3 中学校 */
+			'02.2.6': { icon: 'icons/post-secondary.svg', color: '#C75746' },	/* B3 高等学校 */
+			'02.2.7': { icon: 'icons/books.svg ', color: '#C75746' },	/* B3 特別支援学校 */
+			'02.2.8': { icon: 'icons/books.svg ', color: '#C75746' },	/* B3 社会教育 */
 			'02.2.9': { icon: 'icons/c_yougo.svg', color: '#C75746' },	/* B3 給食 */
 
-	'03': { icon: 'icons/c_waterdoun-watersup.svg', color: '#C75746' },			/* 借金返済 */
-		'03.1': { icon: 'icons/c_watersup.svg', color: '#C75746' },	/* G1 公債費 */
+	'03': { icon: 'icons/dollar.svg ', color: '#C75746' },			/* 借金返済 */
+		'03.1': { icon: 'icons/dollar.svg ', color: '#C75746' },	/* G1 公債費 */
 
 	'04': { icon: 'icons/economy-tourism.svg', color: '#C75746' },			/* 経済・観光 */
 		'04.1': { icon: 'icons/c_nougyou.svg', color: '#C75746' },				/* C1 経済 */
-			'04.1.1': { icon: 'icons/c_kosodate_shien.svg', color: '#C75746' },	/* B1 労働 */
-			'04.1.2': { icon: 'icons/c_sankaku_ikusei.svg', color: '#C75746' },	/* B2 商工業振興 */
+			'04.1.1': { icon: 'icons/labour.svg', color: '#C75746' },	/* B1 労働 */
+			'04.1.2': { icon: 'icons/manufactoring-construction.svg', color: '#C75746' },	/* B2 商工業振興 */
 		'04.2': { icon: 'icons/c_syoukougyou.svg', color: '#C75746' },				/* C2 農畜産業 */
-			'04.2.1': { icon: 'icons/c_kosodate_shien.svg', color: '#C75746' },	/* B1 農業 */
-			'04.2.2': { icon: 'icons/c_sankaku_ikusei.svg', color: '#C75746' },	/* B2 畜産業 */
+			'04.2.1': { icon: 'icons/c_nougyou.svg ', color: '#C75746' },	/* B1 農業 */
+			'04.2.2': { icon: 'icons/pig.svg ', color: '#C75746' },	/* B2 畜産業 */
 		'04.3': { icon: 'icons/harbor.svg', color: '#C75746' },				/* C3 観光 */
-			'04.3.1': { icon: 'icons/c_kosodate_shien.svg', color: '#C75746' },	/* B1 観光 */
+			'04.3.1': { icon: 'icons/island.svg ', color: '#C75746' },	/* B1 観光 */
 		'04.4': { icon: 'icons/harbor.svg', color: '#C75746' },				/* C4 消費者 */
 			'04.4.1': { icon: 'icons/c_kosodate_shien.svg', color: '#C75746' },	/* B1 計量検査 */
 			'04.4.2': { icon: 'icons/c_sankaku_ikusei.svg', color: '#C75746' },	/* B2 消費者対策 */
 
 	'05': { icon: 'icons/our-streets.svg', color: '#C75746' },				/* まちづくり */
-		'05.1': { icon: 'icons/street-lights.svg', color: '#C75746' },		/* D1 土木 */
+		'05.1': { icon: 'icons/construction.svg ', color: '#C75746' },		/* D1 土木 */
 			'05.1.1': { icon: 'icons/c_kosodate_shien.svg', color: '#C75746' },	/* B1 道路 */
 			'05.1.2': { icon: 'icons/c_sankaku_ikusei.svg', color: '#C75746' },	/* B2 河川 */
-			'05.1.3': { icon: 'icons/c_yougo.svg', color: '#C75746' },	/* B3 港湾 */
+			'05.1.3': { icon: 'icons/anchor.svg ', color: '#C75746' },	/* B3 港湾 */
 		'05.2': { icon: 'icons/harbor.svg', color: '#C75746' },				/* D2 街づくり */
 			'05.2.1': { icon: 'icons/c_kosodate_shien.svg', color: '#C75746' },	/* B1 都市計画 */
 			'05.2.2': { icon: 'icons/c_sankaku_ikusei.svg', color: '#C75746' },	/* B2 モノレール */
 			'05.2.3': { icon: 'icons/c_yougo.svg', color: '#C75746' },	/* B3 下水道 */
 		'05.3': { icon: 'icons/housing.svg', color: '#C75746' },			/* D3 住環境 */
-			'05.3.1': { icon: 'icons/c_kosodate_shien.svg', color: '#C75746' },	/* B1 公園 */
+			'05.3.1': { icon: 'community.svg ', color: '#C75746' },	/* B1 公園 */
 			'05.3.2': { icon: 'icons/c_sankaku_ikusei.svg', color: '#C75746' },	/* B2 緑化推進 */
 			'05.3.3': { icon: 'icons/c_yougo.svg', color: '#C75746' },	/* B3 住宅 */
 
@@ -86,9 +86,9 @@ OpenSpending.Styles.Cofog = {
 		'06.4': { icon: 'icons/civilian-action.svg', color: '#C75746' },			/* F4 市民活動 */
 			'06.4.1': { icon: 'icons/c_kosodate_shien.svg', color: '#C75746' },	/* B1 広報 */
 			'06.4.2': { icon: 'icons/c_sankaku_ikusei.svg', color: '#C75746' },	/* B2 市民自治 */
-			'06.4.3': { icon: 'icons/c_yougo.svg', color: '#C75746' },	/* B3 文化・スポーツ振興 */
+			'06.4.3': { icon: 'sports.svg ', color: '#C75746' },	/* B3 文化・スポーツ振興 */
 		'06.5': { icon: 'icons/c_bunnka.svg', color: '#C75746' },			/* F5 安全・安心 */
-			'06.5.1': { icon: 'icons/c_kosodate_shien.svg', color: '#C75746' },	/* B1 交通政策 */
+			'06.5.1': { icon: 'icons/car.svg ', color: '#C75746' },	/* B1 交通政策 */
 			'06.5.2': { icon: 'icons/c_sankaku_ikusei.svg', color: '#C75746' },	/* B2 防災対策 */
 		'06.6': { icon: 'icons/c_bunnka.svg', color: '#C75746' },			/* F6 その他 */
 			'06.6.1': { icon: 'icons/c_kosodate_shien.svg', color: '#C75746' },	/* B1 戸籍管理 */
@@ -98,24 +98,24 @@ OpenSpending.Styles.Cofog = {
 			'06.6.5': { icon: 'icons/c_yougo.svg', color: '#C75746' },	/* B3 その他 */
 
 	'07': { icon: 'icons/c_syoubou_bousai.svg', color: '#C75746' },				/* 消防・救急 */
-		'07.1': { icon: 'c_shyoubou.svg', color: '#C75746' },		/* O1 消防・救急 */
-			'07.1.1': { icon: 'icons/admin-order-safety.svg', color: '#C75746' },	/* B1 消防 */
-			'07.1.2': { icon: 'icons/c_sankaku_ikusei.svg', color: '#C75746' },	/* B2 消防団 */
-			'07.1.3': { icon: 'icons/c_yougo.svg', color: '#C75746' },	/* B3 消防施設 */
-		'07.2': { icon: 'c_shyoubou.svg', color: '#C75746' },		/* O2 消防・救急 */
+		'07.1': { icon: 'icons/c_shyoubou.svg', color: '#C75746' },		/* O1 消防・救急 */
+			'07.1.1': { icon: 'icons/fire-brigade.svg', color: '#C75746' },	/* B1 消防 */
+			'07.1.2': { icon: 'icons/admin-order-safety.svg', color: '#C75746' },	/* B2 消防団 */
+			'07.1.3': { icon: 'icons/fire-brigade.svg ', color: '#C75746' },	/* B3 消防施設 */
+		'07.2': { icon: 'icons/c_shyoubou.svg', color: '#C75746' },		/* O2 消防・救急 */
 		
 	'08': { icon: 'icons/environment.svg', color: '#C75746' },				/* ごみ処理・リサイクル・環境保全 */
 		'08.1': { icon: 'icons/tree.svg', color: '#C75746' },				/* E1 ごみ処理 */
 			'08.1.1': { icon: 'icons/c_kosodate_shien.svg', color: '#C75746' },	/* B1 ごみ処理 */
 			'08.1.2': { icon: 'icons/c_sankaku_ikusei.svg', color: '#C75746' },	/* B2 廃棄物・残土対策 */
 		'08.2': { icon: 'icons/garbage.svg', color: '#C75746' },			/* E2 リサイクル */
-			'08.2.1': { icon: 'icons/c_kosodate_shien.svg', color: '#C75746' },	/* B1 リサイクル */
-		'08.3': { icon: 'icons/garbage.svg', color: '#C75746' },			/* E3 環境保全*/
-			'08.3.1': { icon: 'icons/c_kosodate_shien.svg', color: '#C75746' },	/* B1 し尿処理 */
+			'08.2.1': { icon: 'icons/garbage.svg', color: '#C75746' },	/* B1 リサイクル */
+		'08.3': { icon: 'icons/environment.svg', color: '#C75746' },			/* E3 環境保全*/
+			'08.3.1': { icon: 'icons/toilet.svg ', color: '#C75746' },	/* B1 し尿処理 */
 		
 	'09': { icon: 'icons/c_government4.svg', color: '#C75746' },				/* その他 */
 		'09.1': { icon: 'icons/c_government4.svg', color: '#C75746' },		/* H1 その他 */
-			'09.1.1': { icon: 'icons/c_kosodate_shien.svg', color: '#C75746' },	/* B1 基金償還等 */
+			'09.1.1': { icon: 'icons/dollar.svg ', color: '#C75746' },	/* B1 基金償還等 */
 			'09.1.2': { icon: 'icons/c_sankaku_ikusei.svg', color: '#C75746' },	/* B2 災害復旧 */
 		'09.2': { icon: 'icons/c_government4.svg', color: '#C75746' },		/* H1 その他 */
 
