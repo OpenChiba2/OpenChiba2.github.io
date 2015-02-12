180d060a99c360478bb7586db95589c8e3327ea8
chi-ba <yoshimgtsu@gmail.com>
2015-02-12 09:27:06 +0900

    Update cofog.js

--- js/cofog.js
+++ js/cofog.js
@@ -117,6 +117,5 @@ OpenSpending.Styles.Cofog = {
 		'09.1': { icon: 'icons/c_government4.svg', color: '#C75746' },		/* H1 その他 */
 			'09.1.1': { icon: 'icons/dollar.svg ', color: '#C75746' },	/* B1 基金償還等 */
 			'09.1.2': { icon: 'icons/c_sankaku_ikusei.svg', color: '#C75746' },	/* B2 災害復旧 */
-		'09.2': { icon: 'icons/c_government4.svg', color: '#C75746' },		/* H1 その他 */
 
 };
